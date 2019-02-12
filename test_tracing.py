ac9ca5932a0b6b7da6f7ed7eb504607510eacbf2
songbowen <songbowen@qiyi.com>
2019-02-12 11:35:59 +0800

    append test

--- test_tracing.py
+++ test_tracing.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import uuid
 
 import flask
 import pytest
@@ -11,17 +12,31 @@ def app(request):
     app = flask.Flask(request.module.__name__)
     app.testing = True
     Tracing(app)
-    return app
 
-
-def test_add_request_id(app):
     @app.route('/')
     def index():
         return 'hello world.'
 
+    return app
+
+
+def test_add_request_id(app):
     with app.test_client() as c:
         rv = c.get('/')
         assert 'X-Request-ID' in rv.headers
         request_id = rv.headers.get('X-Request-ID')
         assert len(request_id) == 36
         assert len(request_id.split('-')) == 5
+
+
+def test_append_trace_id(app):
+    with app.test_client() as c:
+        original_id = uuid.uuid4()
+        rv = c.get('/', headers={
+            'X-Request-ID': original_id
+        })
+        assert 'X-Request-ID' in rv.headers
+        request_ids = rv.headers.get('X-Request-ID')
+        assert len(request_ids.split(',')) == 2
+        old, new = request_ids.split(',')
+        assert old == str(original_id)
